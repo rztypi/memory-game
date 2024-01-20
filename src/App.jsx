94c979fbe1ee59318b515da1a82b37b303795ae2
rztypi <rztypiraman@gmail.com>
2024-01-20 11:28:42 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__title">
+          <i className="bi bi-sd-card-fill"></i> Memory Card
+        </h1>
+      </header>
+      <main></main>
+      <footer className="footer">
+        <a href="https://github.com/rztypi" className="gh-link">
+          <i className="bi bi-github"></i> rztypi
+        </a>
+      </footer>
+    </>
+  );
+};
+
+export default App;
